fbf4ca95edc0c9fcef929652c9d923f6ea8f8af4
olivia <opierez@gmail.com>
2023-01-10 09:47:41 -0500

    commit tree f8fe463fa3e8ddd6cf0e2bc188f35989271b986f

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match arr
+        arr.filter {|x| x.chars.sort == @word.chars.sort}
+    end
+
+end
+
+# binding.pry
